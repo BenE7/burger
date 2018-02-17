222aa140718ce0c628a5b109b38d823da42d552e
Ben Evans <benevans@Bens-MacBook-Pro.local>
2018-02-17 12:09:28 -0500

    herokustuff

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
 
-    user: "root",
-    password: "",
+    user: "wi7gibbb812f1h7j",
+    password: "lja0t3kscu4mch3o",
     database: "burgers_db"
 });
 
